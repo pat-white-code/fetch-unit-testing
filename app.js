f9fa2f3f49fdb19e2c23ca13f91fe22eae1909f1
pat-white-code <pat.k.white@gmail.com>
2019-12-23 14:00:05 -0600

    makes fetch return array of beers that uses text field to send a query to the request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let input = document.getElementById('input');
+let abv_gt = document.getElementById('abv_gt');
 
 const thenBeer = () => {
   let id = input.value;
@@ -12,29 +13,58 @@ const thenBeer = () => {
 }
 
 const asyncBeer = async() => {
-  let id = input.value;
-  let response = await fetch('https://api.punkapi.com/v2/beers/'+id);
+  let abv = abv_gt.value;
+  let response = await fetch(`https://api.punkapi.com/v2/beers/?abv_gt=${abv}`);
   let results = await response.json();
-  let beer = results[0];
-  displayBeer(beer);
-  console.log(beer)
+  let beers = results;
+  displayBeers(beers);
+  console.log(beers);
 }
 
 const submit = document.getElementById('submit');
 submit.addEventListener('click', asyncBeer)
 
+const displayBeers = (beersArr) => {
+  beersArr.forEach(beer => renderBeer(beer));
+}
 
-const displayBeer = (beer) => {
-  let imgContainer = document.getElementById('img-container');
-  imgContainer.innerHTML = '';
-  const name = document.getElementById('name')
-  name.innerText = beer.name;
-  const abv = document.getElementById('abv');
-  abv.innerText = beer.abv;
-  const desc = document.getElementById('desc');
-  desc.innerText = beer.description;
+// const displayBeer = (beer) => {
+//   let imgContainer = document.getElementById('img-container');
+//   imgContainer.innerHTML = '';
+//   const name = document.getElementById('name')
+//   name.innerText = beer.name;
+//   const abv = document.getElementById('abv');
+//   abv.innerText = beer.abv;
+//   const desc = document.getElementById('desc');
+//   desc.innerText = beer.description;
+//   let img = document.createElement('img');
+//   img.src = beer.image_url;
+//   img.classList.add('beer-img')
+//   imgContainer.appendChild(img)
+// }
+
+const renderBeer = (beer) => {
+  let imgContainer = document.createElement('div');
+  imgContainer.classList.add('img-container');
+  let infoContainer = document.createElement('div');
+  infoContainer.classList.add('info-container');
+  let beerSection = document.getElementById('beers')
+  let card = document.createElement('div');
+  card.classList.add('beer-card');
   let img = document.createElement('img');
   img.src = beer.image_url;
-  img.classList.add('beer-img')
-  imgContainer.appendChild(img)
+  img.classList.add('beer-img');
+  let h1 = document.createElement('h1');
+  h1.innerText = beer.name;
+  let h2 = document.createElement('h2');
+  h2.innerText = beer.abv;
+  let p = document.createElement('p');
+  p.innerText = beer.description;
+  imgContainer.appendChild(img);
+  infoContainer.appendChild(h1);
+  infoContainer.appendChild(h2);
+  infoContainer.appendChild(p);
+  card.appendChild(imgContainer);
+  card.appendChild(infoContainer);
+  beerSection.appendChild(card);
 }
