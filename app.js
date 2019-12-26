ad06939cc20667fa983fa9d8051dc4658828dfb6
pat-white-code <pat.k.white@gmail.com>
2019-12-26 13:00:39 -0600

    makes fetch on window load, makes so empty fields are omitted from request param

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 let input = document.getElementById('input');
 let abv_gt = document.getElementById('abv_gt');
+let food = document.getElementById('food');
+let ibu_gt = document.getElementById('ibu_gt');
+let beerSection = document.getElementById('beers');
 
-const thenBeer = () => {
-  let id = input.value;
-  fetch('https://api.punkapi.com/v2/beers/'+id)
-    .then(res => res.json())
-    .then(data => {
-      displayBeer(data[0])
-      console.log(data[0])
-    }
-      )
-}
+// const thenBeer = () => {
+//   let id = input.value;
+//   fetch('https://api.punkapi.com/v2/beers/'+id)
+//     .then(res => res.json())
+//     .then(data => {
+//       displayBeer(data[0])
+//       console.log(data[0])
+//     }
+//       )
+// }
 
 const asyncBeer = async() => {
-  let abv = abv_gt.value;
-  let response = await fetch(`https://api.punkapi.com/v2/beers/?abv_gt=${abv}`);
+  beerSection.innerHTML = '';
+  let URL = 'https://api.punkapi.com/v2/beers/'
+  let optionsArr = [[abv_gt.value, 'abv_gt='],[ibu_gt.value, 'ibu_gt='],[food.value, 'food=']]
+  let url = composeURL(optionsArr, URL);
+  console.log(url);
+  let response = await fetch(url);
   let results = await response.json();
   let beers = results;
   displayBeers(beers);
   console.log(beers);
 }
 
-const submit = document.getElementById('submit');
-submit.addEventListener('click', asyncBeer)
+const composeURL = (arr, string) => {
+  arr.forEach(item => {
+    if(item[0] && string.length > 33) {
+      string += '&' + item[1] + item[0]
+    } else if (item[0]) {
+      string += '?' + item[1] + item[0]
+    }
+  } )
+  return string
+}
+
+abv_gt.addEventListener('change', asyncBeer);
+ibu_gt.addEventListener('change', asyncBeer);
+food.addEventListener('change', asyncBeer);
 
 const displayBeers = (beersArr) => {
   beersArr.forEach(beer => renderBeer(beer));
@@ -45,26 +64,48 @@ const displayBeers = (beersArr) => {
 
 const renderBeer = (beer) => {
   let imgContainer = document.createElement('div');
-  imgContainer.classList.add('img-container');
   let infoContainer = document.createElement('div');
-  infoContainer.classList.add('info-container');
-  let beerSection = document.getElementById('beers')
   let card = document.createElement('div');
-  card.classList.add('beer-card');
   let img = document.createElement('img');
+  let h1 = document.createElement('h1');
+  let h2 = document.createElement('h2');
+  let p = document.createElement('p');
+  let ul = document.createElement('ul');
+  let food = beer.food_pairing.map(pairingLis);
+
+  imgContainer.classList.add('img-container');
+  infoContainer.classList.add('info-container');
+  card.classList.add('beer-card');
+
   img.src = beer.image_url;
   img.classList.add('beer-img');
-  let h1 = document.createElement('h1');
+  
   h1.innerText = beer.name;
-  let h2 = document.createElement('h2');
   h2.innerText = beer.abv;
-  let p = document.createElement('p');
   p.innerText = beer.description;
+  
+  food.forEach(pairing => ul.appendChild(pairing));
+  
   imgContainer.appendChild(img);
+  
   infoContainer.appendChild(h1);
   infoContainer.appendChild(h2);
   infoContainer.appendChild(p);
+  infoContainer.appendChild(ul);
+  
   card.appendChild(imgContainer);
   card.appendChild(infoContainer);
+
   beerSection.appendChild(card);
+}
+
+const pairingLis = (food) => {
+  let li = document.createElement('li')
+  li.innerText = food
+  return li
+}
+
+window.onload = (event) => {
+  console.log('window is loaded', event);
+  asyncBeer();
 }
